c67a5a74864e157f6d90ba7a73c65c51148bbf12
Magdiel Juárez Guerrero <mJuarez@mjGuerrero-shared.local>
2018-05-07 13:04:35 -0500

    Initial commit with WS Architecture

---
+++ app/Http/Controllers/API_Dependencies/GoogleAPIController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+
+
+class GoogleAPIController extends Controller {
+
+
+	/**
+	 *
+	 * @param
+	 * @return
+	 */
+	public function getFuelStations() {
+
+	}
+
+	/**
+	 *
+	 * @param
+	 * @return
+	 */
+	public function getParkings() {
+
+	}
+
+	/**
+	 *
+	 * @param
+	 * @return
+	 */
+	public function getCarwashes() {
+
+	}
+
+	/**
+	 *
+	 * @param
+	 * @return
+	 */
+	public function getFuelStations() {
+
+	}
+
+	/**
+	 *
+	 * @param
+	 * @return
+	 */
+	public function getVulcanisers() {
+
+	}
+
+	/**
+	 *
+	 * @param
+	 * @return
+	 */
+	public function getElectricists() {
+
+	}
+
+	/**
+	 *
+	 * @param
+	 * @return
+	 */
+	public function getMechanics() {
+
+	}
+
+	
+
+
+
+}
---
+++ app/Http/Controllers/AirQualityController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+
+
+class AirQualityController extends Controller {
+
+	/**
+	 * Método que consulta la calidad del aire utilizando un API externa utilizando coordenadas. Su url:
+	 * http://aqicn.org/json-api/doc/#api-_
+	 * @param $lat Latitud de la ubicación donde se quiere obtener la calidad del aire
+ 	 * @param $lat Longitud de la ubicación donde se quiere obtener la calidad del aire
+	 * @return Un objeto de tipo JSON que contiene la información solicitada
+	 */
+	public function getAirQuality( $lat, $long ) {
+
+		//$lat = 19.42461;
+		//$long = -99.119594;
+
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		    CURLOPT_RETURNTRANSFER => true,
+		    CURLOPT_CONNECTTIMEOUT => 10,
+		    CURLOPT_URL => 'https://api.waqi.info/feed/geo:'.$lat.';'.$long.'/?token=1f914c9b1ffbf3b4ac82641d116ee93a0011ea2d'
+		));
+
+		$resp = curl_exec($curl);
+		curl_close($curl);
+
+		$resp = json_decode($resp);
+		header('Content-Type: application/json');
+		return response() -> json([
+			'status' => $resp->status,
+			'data' => ['aqi' => $resp->data->aqi ]], 201);
+
+	}
+}
+
