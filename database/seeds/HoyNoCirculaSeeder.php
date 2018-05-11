f2cc4e8de267fa6ef82973cd7b0e8e22eba2275b
Jess <jessica.jaime@red-wolf.com.mx>
2018-05-11 17:51:15 -0500

    Seeders casi listos

--- database/seeds/HoyNoCirculaSeeder.php
+++ database/seeds/HoyNoCirculaSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 use App\HoyNoCircula;
 
 class HoyNoCirculaSeeder extends Seeder
@@ -14,11 +16,11 @@ class HoyNoCirculaSeeder extends Seeder
     {
         $faker = Faker::create();
         for ($x = 1; $x < 11; $x++) {
-            DB::table('calidad_aire')->insert([
-                'ctlg_hologramas_id_ctlg_hologramas'=> rand(1, 10),
-                'horario_id_horario'=> rand(1, 10),
-                'ctlg_id_ctlg_hoy_no_circula'=> rand(1, 10),
-                'vehiculo_id_vehiculo'=> rand(1, 10),
+            DB::table('hoy_no_circula')->insert([
+                'ctlg_hologramas_id_ctlg_hologramas'=> rand(1, 5),
+                'horario_id_horario'=> rand(1, 5),
+                'ctlg_id_ctlg_hoy_no_circula'=> rand(1, 5),
+                'vehiculo_id_vehiculo'=> rand(1, 5),
                 'created_at' => $faker->dateTime,
                 'updated_at' => $faker->dateTime,
             ]);
