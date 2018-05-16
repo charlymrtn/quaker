34a5bbe5c4c08f31ab29868c8c6306dabe2a2446
Jess <jessica.jaime@red-wolf.com.mx>
2018-05-16 13:35:14 -0500

    api inicio se sesión y registro funcionando

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,19 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['prefix' => 'quaker', 'middleware' => 'auth:api'], function() {
+    Route::resource('vehiculo', 'VehiculoController');
+    Route::resource('calidadAire', 'CalidadAireController');
+});
+//Auth::routes();
+
+Route::post('register', 'Auth\RegisterController@register');
+Route::get('login', 'Auth\LoginController@login');
+
+Route::group(['middleware' => ['auth:api']], function() {
+
+    //Route::get('/getAirQuality/{lat}/{long}', 'TestController@getAirQuality');
+
+});
+
+Route::get('/getAirQuality/{lat}/{long}', 'API_Dependencies\AirQualityController@getAirQuality');
